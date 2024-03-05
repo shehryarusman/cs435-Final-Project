2103706969b6249dde5e4088366b4f4a06591b98
Alyque Farishta <alyquefarishta@n3-84-68.dhcp.drexel.edu>
2024-03-05 10:45:27 -0500

    fixed double agent

--- double_q_learning_agent.py
+++ double_q_learning_agent.py
@@ -1,10 +1,6 @@
 import numpy as np
 import random
 
-def flipCoin():
-    r = random.random()
-    return r < 0.5
-
 class DoubleQLearningAgent:
     def __init__(self, state_space_size, action_space_size, learning_rate=0.1, discount_rate=0.95, exploration_rate=1.0, exploration_decay=0.99, min_exploration_rate=0.01):
         self.state_space_size = state_space_size
@@ -28,7 +24,7 @@ class DoubleQLearningAgent:
             return np.argmax(self.q_table2[state])
 
     def update_q_table(self, state, action, new_state, reward):
-        if flipCoin:
+        if random.random() < 0.5:
             future_rewards = np.max(self.q_table2[new_state])
             current_q_value = self.q_table1[state, action]
             new_q_value = current_q_value + self.learning_rate * (reward + self.discount_rate * future_rewards - current_q_value)
